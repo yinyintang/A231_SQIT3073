36153317345a5dec3662419c6c385c673a5ec4e3
yinyintang <tangyinyin_00@hotmail.com>
2023-11-19 21:40:03 +0800

    Commmit

---
+++ W3/6_Dictionaries.py
@@ -0,0 +1,44 @@
+
+# Create a dictionary of student information
+student = {
+    "name": "Alice",
+    "age": int(20),
+    "major": "Computer Science",
+    "grades": {
+        "math": 95,
+        "english": 88,
+        "history": 92
+    }
+}
+
+# Access dictionary values by keys
+student_name = student["name"]
+student_age = student["age"]
+
+# Modify dictionary values
+student["age"] = 21
+student["grades"]["math"] = 97
+
+# Add a new key-value pair
+student["gender"] = "Female"
+
+# Check if a key exists in the dictionary
+has_major = "major" in student
+has_height = "height" in student
+
+# Get the list of keys and values
+keys = student.keys()
+values = student.values()
+
+# Iterate through the dictionary
+print("Student Information:")
+for key, value in student.items():
+    print(f"{key}: {value}")
+
+# Remove a key-value pair
+del student["grades"]
+
+# Print the updated dictionary
+print("\nStudent Information after removing 'grades':")
+for key, value in student.items():
+    print(f"{key}: {value}")
