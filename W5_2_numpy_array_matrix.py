849310d727a499b467019d6965fb60640dd2c464
yinyintang <tangyinyin_00@hotmail.com>
2023-11-20 11:42:12 +0800

    Commit

--- W5_2_numpy_array_matrix.py
+++ W5_2_numpy_array_matrix.py
@@ -1,12 +1,12 @@
 import os
 
 try:
-    # Attempt to clear the screen for macOS
-    os.system('clear')
+    # Attempt to clear the screen for Windows
+    os.system('cls')
 except:
     try:
-        # Attempt to clear the screen for Windows if the first try fails
-        os.system('cls')
+        # Attempt to clear the screen for MacOS if the first try fails
+        os.system('clear')
     except:
         # Output an error message if both attempts fail
         print("Unable to clear the screen.")
@@ -14,15 +14,15 @@ except:
 
 import numpy as np  # Import the NumPy library for numerical operations
 
-# Create 1D NumPy arrays
-# Initializing 1-dimensional arrays 
-arr1 = np.array([1, 2, 3, 4, 5])
-arr2 = np.array([5, 4, 3, 2, 1])
+# Create 2D NumPy arrays
+# Initializing 2-dimensional arrays
+arr1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+arr2 = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
 
 # Access elements
 # Accessing the first and last elements to demonstrate element retrieval.
-first_element = arr1[0]
-last_element = arr1[-1]
+first_element = arr1[0, 0]
+last_element = arr1[-1, -1]
 
 # Perform basic mathematical operations
 
@@ -67,8 +67,8 @@ log_values = np.log(arr1)
 
 # Display results
 # Outputting the results to the console.
-print(f"Original array 1: {arr1}")
-print(f"Original array 2: {arr2}")
+print(f"Original array 1:\n{arr1}")
+print(f"Original array 2:\n{arr2}")
 print(f"First element: {first_element}")
 print(f"Last element: {last_element}")
 print(f"Sum of elements: {sum_elements}")
@@ -78,13 +78,9 @@ print(f"Minimum element: {min_element}")
 print(f"Maximum element: {max_element}")
 print(f"Standard Deviation: {std_dev}")
 print(f"Variance: {variance}")
-print(f"Element-wise Addition: {elementwise_add}")
-print(f"Element-wise Subtraction: {elementwise_sub}")
-print(f"Element-wise Multiplication: {elementwise_mul}")
-print(f"Element-wise Division: {elementwise_div}")
-print(f"Sine values: {sine_values}")
-print(f"Natural Logarithm values: {log_values}")
-
-
-# Created by Dr Aamir Adeeb
-# Contact for more info at aamir@uum.edu.my
+print(f"Element-wise Addition:\n{elementwise_add}")
+print(f"Element-wise Subtraction:\n{elementwise_sub}")
+print(f"Element-wise Multiplication:\n{elementwise_mul}")
+print(f"Element-wise Division:\n{elementwise_div}")
+print(f"Sine values:\n{sine_values}")
+print(f"Natural Logarithm values:\n{log_values}")
