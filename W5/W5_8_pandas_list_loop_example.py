c67fb9ae807510a4fd0a35c6a0d28a4ce51fbeaa
yinyintang <tangyinyin_00@hotmail.com>
2023-11-22 15:18:31 +0800

    Commit

---
+++ W5/W5_8_pandas_list_loop_example.py
@@ -0,0 +1,31 @@
+import os
+
+try:
+    # Attempt to clear the screen for Windows
+    os.system('cls')
+except:
+    try:
+        # Attempt to clear the screen for macOS if the first try fails
+        os.system('clear')
+    except:
+        # Output an error message if both attempts fail
+        print("Unable to clear the screen.")
+
+
+import pandas as pd
+
+# Initialize an empty list to store data
+data_int_id = []
+
+# Generate student names and IDs in a loop
+for i in range(1, 6):  # Assuming we have 5 students
+    student_name = f"Student_{i}"
+    student_id = 1000 + i
+    data_int_id.append([student_id, student_name])
+
+# Create a DataFrame using the list
+df_int_id = pd.DataFrame(data_int_id, columns=['Student_ID', 'Student_Name'])
+
+# Display the DataFrame
+print("DataFrame using Integer IDs:")
+print(df_int_id)
